763865730575f3dfbee6a32c5e8e13d547df63bd
avatarr05 <eya.cherif@mila.quebec>
2025-05-09 13:05:06 +0200

    Update Split_data.py

--- scripts/Split_data.py
+++ scripts/Split_data.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import warnings
-from src.utils_data import *
+
 
 import subprocess
 warnings.filterwarnings('ignore')  # ignore warnings, like ZeroDivision
@@ -9,23 +9,22 @@ warnings.filterwarnings('ignore')  # ignore warnings, like ZeroDivision
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
+
+from src.utils_data import *
+
 directory_path = os.path.join(project_root, "Splits")
 directory_path_Ds = os.path.join(project_root, "Datasets")
 directory_path_Ds_unlb = os.path.join(directory_path_Ds, "unlb")
 
-# # Install Git LFS (only needed once)
-# !git lfs install
+import subprocess
 
-# # Clone the entire dataset repo
-# !git clone https://huggingface.co/datasets/avatar5/DiverseSpecLib {directory_path_Ds}
-
-# Run Git LFS install
-subprocess.run(["git", "lfs", "install"], check=True)
+# # Run Git LFS install
+# subprocess.run(["git", "lfs", "install"], check=True)
 
 # Clone the repo into your desired directory
 subprocess.run([
     "git", "clone",
-    "https://huggingface.co/datasets/avatar5/DiverseSpecLib",
+    "https://huggingface.co/datasets/Avatarr05/GreenHySpectra",
     directory_path_Ds
 ], check=True)
 
@@ -33,18 +32,6 @@ subprocess.run([
 num_splits = 20  # Number of output splits
 chunk_size = 5000  # Tune based on your memory constraints
 
-os.makedirs(directory_path, exist_ok=True)  # Create the output folder if it doesn't exist
-# split_csvs_with_proportions_sequential(directory_path_Ds, directory_path, num_splits, chunk_size)
-split_parquets_with_proportions_sequential(directory_path_Ds, directory_path, num_splits, chunk_size)
-subprocess.run([
-    "git", "clone",
-    "https://huggingface.co/datasets/avatar5/DiverseSpecLib",
-    directory_path_Ds
-], check=True)
-
-
-num_splits = 20  # Number of output splits
-chunk_size = 5000  # Tune based on your memory constraints
 
 os.makedirs(directory_path, exist_ok=True)  # Create the output folder if it doesn't exist
 split_csvs_with_proportions_sequential(directory_path_Ds_unlb, directory_path, num_splits, chunk_size)
