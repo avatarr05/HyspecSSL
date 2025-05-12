d6c25c560493ff99760d646915e615e056e3d9e4
avatarr05 <eya.cherif@mila.quebec>
2025-05-12 10:24:11 +0200

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-APP_ROOT = Path(__file__).resolve().parent
-PROJECT_ROOT = APP_ROOT.parent
-CONFIGS_PATH = PROJECT_ROOT / "configs"
-DATA_DIR = PROJECT_ROOT / "data"
-SCRIPTS_DIR = PROJECT_ROOT / "scripts"
